6e227877a9ab9870de170164855f4bbaf46ca15f
Aleksa Ristic <aristiccitsira@gmail.com>
2024-05-16 21:32:34 +0200

    Added date formating

--- src/ui/LSReleaseLogItem.tsx
+++ src/ui/LSReleaseLogItem.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import { ILSReleaseLogItemProps } from "../interfaces/ILSReleaseLogItemProps"
+import moment from "moment"
 
 export const LSReleaseLogItem = (props: ILSReleaseLogItemProps): React.JSX.Element => {
     return (
         <div className={`ls-release-log-item-wrapper`}>
             <div>
-                <div>{props.pullRequest.closed_at.toString()}</div>
+                <div>{moment(props.pullRequest.closed_at).format("DD.MM.yyyy (HH:mm)").toString()}</div>
             </div>
             <div>{props.pullRequest.body}</div>
         </div>
