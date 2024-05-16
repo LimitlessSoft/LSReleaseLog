3c916f0839fd31d8dc26a0bcfa5dd7412f4cc0c8
Aleksa Ristic <aristiccitsira@gmail.com>
2024-05-16 20:54:32 +0200

    Uninstalled octokit

--- src/ui/LSReleaseLog.tsx
+++ src/ui/LSReleaseLog.tsx
@@ -1,8 +1,24 @@
-import React from "react"
+import React, { useEffect } from "react"
 import "../styles/LSReleaseLog.css"
 import { LSReleaseLogItem } from "./LSReleaseLogItem"
+import { ILSReleaseLogProps } from "../interfaces/ILSReleaseLogProps"
+import { Octokit } from "octokit"
 
-export const LSReleaseLog = (): React.JSX.Element => {
+export const LSReleaseLog = (props: ILSReleaseLogProps): React.JSX.Element => {
+
+    useEffect(() => {
+        const octokit = new Octokit({
+            auth: process.env.REACT_APP_GITHUB_TOKEN
+        })
+
+        octokit.repos.list({
+            owner: "LimitlessSoft",
+            repo: "termodom--ecosystem"
+        }).then((response: any) => {
+            console.log(response)
+        })
+    
+    })
     return (
         <div className={`ls-release-log-wrapper`}>
             <LSReleaseLogItem />
