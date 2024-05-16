aefcde93cf949153e7a819c9742794e67608c8b3
Aleksa Ristic <aristiccitsira@gmail.com>
2024-05-16 21:12:46 +0200

    Stage

--- src/ui/LSReleaseLog.tsx
+++ src/ui/LSReleaseLog.tsx
@@ -11,7 +11,7 @@ export const LSReleaseLog = (props: ILSReleaseLogProps): React.JSX.Element => {
     })
 
     useEffect(() => {
-        octokit.request('GET /repos/{owner}/{repo}/pulls?state=closed', {
+        octokit.request('GET /repos/{owner}/{repo}/pulls?state=closed&base=master', {
             owner: props.repoOwner,
             repo: props.repoName,
 
