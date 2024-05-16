12b79871e841f8f1949f113e67528cf7f78c0024
Aleksa Ristic <aristiccitsira@gmail.com>
2024-05-16 20:55:21 +0200

    Returned octokit

--- src/ui/LSReleaseLog.tsx
+++ src/ui/LSReleaseLog.tsx
@@ -2,23 +2,13 @@ import React, { useEffect } from "react"
 import "../styles/LSReleaseLog.css"
 import { LSReleaseLogItem } from "./LSReleaseLogItem"
 import { ILSReleaseLogProps } from "../interfaces/ILSReleaseLogProps"
-import { Octokit } from "octokit"
 
 export const LSReleaseLog = (props: ILSReleaseLogProps): React.JSX.Element => {
 
     useEffect(() => {
-        const octokit = new Octokit({
-            auth: process.env.REACT_APP_GITHUB_TOKEN
-        })
-
-        octokit.repos.list({
-            owner: "LimitlessSoft",
-            repo: "termodom--ecosystem"
-        }).then((response: any) => {
-            console.log(response)
-        })
-    
+        fetch
     })
+    
     return (
         <div className={`ls-release-log-wrapper`}>
             <LSReleaseLogItem />
